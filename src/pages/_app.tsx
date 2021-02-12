4e6b8d1f39055a44c8996e4850c907814701b824
bruno <bruno.tanaka@e-deploy.com.br>
2021-02-12 12:27:06 -0400

    show top loading when change routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 import GlobalStyle from '../styles/globalStyle'
 import Layout from '../components/Layout'
+import useLoading from '../hooks/useLoading'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   const { session } = pageProps
+  useLoading()
 
   const canShowLayout = !router.pathname.startsWith('/login')
 
